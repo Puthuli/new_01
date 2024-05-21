82b1fb901b803e010c652eb330de719642d6950d
216095D <puthuliperera@gmail.com>
2024-05-21 21:42:18 +0530

    new code created

---
+++ new02.py
@@ -0,0 +1,9 @@
+# Get numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+result = num1 + num2
+
+# Display the result
+print("Sum:", result)
