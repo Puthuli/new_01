be8dc6afee61cf9aa4bfb8fc066664d07965e2eb
Duvindu Sahan <100335279+duvindusahan@users.noreply.github.com>
2024-05-21 23:08:05 +0530

    Update new02.py

--- new02.py
+++ new02.py
@@ -6,5 +6,5 @@ num2 = float(input("Enter second number: "))
 result = num1 + num2
 
 # Display the result
+print("Here is the collaborated github project for calculating sum of numbers.")
 print("Sum:", result)
-print("hello")
